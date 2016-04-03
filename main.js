e0a3bce7b39ee07e98d72e9721cc952ebab5ed88
Andrew London <london.andrewd@gmail.com>
2016-04-03 18:28:45 -0400

    test for safari

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-
+// Safari CSS and Webkit Google Chrome
+if ($.browser.webkit) {
+   $("#element").css('top', '2px');
+} else if ( $.browser.safari ) //not fully supported on 1.7 jQuery {
+   $("#element").css('top', '2px');
+// Opera CSS
+} else if ( $.browser.opera ) {
+   $("#element").css('top', '2px');
+// Internet Explorer CSS
+} else if ( $.browser.msie ) {
+   $("#element").css('top', '2px');
+// Mozilla FireFox CSS
+} else if ( $.browser.mozilla ) {
+   $("#element").css('top', '2px');
+// Normal Revert, careful and note your the use of !important
+} else {
+   $("#element").css('top', '2px');
+   // You can have normal JavaScript between these too
+   document.getElementById("element").style.top="2px";
+}
 
 
 $(document).ready(function() {
